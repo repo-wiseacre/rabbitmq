86541f40ccb2ddbd2a16bdfec9d18d510f9a2887
Shuvajit Ghosh <ghosh.shuvajit@gmail.com>
2020-06-19 16:41:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,34 @@
 
 var log4js = require('log4js');
 log4js.configure({
-  appenders: [
-    { type: 'console' },
-    { type: 'file', filename: 'logs/cheese.log', category: 'cheese' }
-  ]
-});
+    "appenders": {
+      "access": {
+        "type": "dateFile",
+        "filename": "log/access.log",
+        "pattern": "-yyyy-MM-dd",
+        "category": "http"
+      },
+      "app": {
+        "type": "file",
+        "filename": "log/app.log",
+        "maxLogSize": 10485760,
+        "numBackups": 3
+      },
+      "errorFile": {
+        "type": "file",
+        "filename": "log/errors.log"
+      },
+      "errors": {
+        "type": "logLevelFilter",
+        "level": "ERROR",
+        "appender": "errorFile"
+      }
+    },
+    "categories": {
+      "default": { "appenders": [ "app", "errors" ], "level": "DEBUG" },
+      "http": { "appenders": [ "access"], "level": "DEBUG" }
+    }
+  });
 
 var express = require('express'),
     app     = express(),
